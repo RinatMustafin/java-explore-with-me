168fa9e2454e6e5c1bcb0941d1cff10d1670e817
Rinat Mustafin <rin.mustafin2010@yandex.ru>
2025-08-08 20:44:57 +0500

    stat svc check

--- ewm-stats/ewm-stats-service/src/main/java/ru/practicum/stats/controller/StatsController.java
+++ ewm-stats/ewm-stats-service/src/main/java/ru/practicum/stats/controller/StatsController.java
@@ -1,7 +1,6 @@
 package ru.practicum.stats.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.stats.EndpointHit;
