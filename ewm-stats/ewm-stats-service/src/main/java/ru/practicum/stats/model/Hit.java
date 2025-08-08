2922ca682203b962450f383763bf68440ecffeaf
Rinat Mustafin <rin.mustafin2010@yandex.ru>
2025-08-08 06:33:55 +0500

    stat svc check

--- ewm-stats/ewm-stats-service/src/main/java/ru/practicum/stats/model/Hit.java
+++ ewm-stats/ewm-stats-service/src/main/java/ru/practicum/stats/model/Hit.java
@@ -17,12 +17,8 @@ public class Hit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String app;
-
     private String uri;
-
     private String ip;
-
     private LocalDateTime timestamp;
 }
