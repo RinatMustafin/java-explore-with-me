f4ce9f9bef0e45c1045be726497e2be3ceca3aa5
Rinat Mustafin <rin.mustafin2010@yandex.ru>
2025-08-09 10:35:49 +0500

    stat svc check, fix mistake

--- ewm-stats/ewm-dto/src/main/java/ru/practicum/stats/EndpointHit.java
+++ ewm-stats/ewm-dto/src/main/java/ru/practicum/stats/EndpointHit.java
@@ -1,16 +1,19 @@
 package ru.practicum.stats;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class EndpointHit {
-    private Long id;
-    private String app;
-    private String uri;
-    private String ip;
-    private String timestamp; // формат "yyyy-MM-dd HH:mm:ss"
+    Long id;
+    String app;
+    String uri;
+    String ip;
+    String timestamp; // формат "yyyy-MM-dd HH:mm:ss"
 }
--- ewm-stats/ewm-dto/src/main/java/ru/practicum/stats/ViewStats.java
+++ ewm-stats/ewm-dto/src/main/java/ru/practicum/stats/ViewStats.java
@@ -1,14 +1,17 @@
 package ru.practicum.stats;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ViewStats {
-    private String app;
-    private String uri;
-    private Long hits;
+    String app;
+    String uri;
+    Long hits;
 }
